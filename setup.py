c7a8409ead3ca51df1e0aa3b8bf4628862bf33be
jordiabante <jordi.abante@ub.edu>
2025-05-31 15:39:34 +0200

    Updated authors of repo.

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='cagennet',
     version='0.1.0',
-    author='Jordi Abante',
-    author_email='jordi.abante@ub.edu',
+    author='Jordi Abante, Berta Ros',
+    author_email='jordi.abante@ub.edu, bertaros@ub.edu',
     description='Calcium Generative Networks (CaGenNet): a package with deep generative models for Ca imaging data.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
